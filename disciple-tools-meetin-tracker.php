7191fd9e167e0a4237e13ab652fb55b32f0436ac
Chance Cox <chance.cox@gmail.com>
2019-06-12 18:23:25 -0500

    init

---
+++ disciple-tools-meetin-tracker.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Disciple Tools - Meeting Tracker
+ * Plugin URI: https://github.com/DiscipleTools/disciple-tools
+ * Description: Disciple Tools - Meeting Tracker, Track meetings with contacts.
+ * Version:  0.2
+ * Author URI: https://github.com/DiscipleTools
+ * GitHub Plugin URI: https://github.com/chancemcox/Disciple-Tools-Meeting-Tracker
+ * Requires at least: 4.7.0
+ * (Requires 4.7+ because of the integration of the REST API at 4.7 and the security requirements of this milestone version.)
+ * Tested up to: 4.9
+ *
+ * @package Disciple_Tools
+ * @link    https://github.com/DiscipleTools
+ * @license GPL-2.0 or later
+ *          https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
