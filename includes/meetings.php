1728f4c3dbc8071d54a6ca07916980103c9de736
Chance Cox <chance.cox@gmail.com>
2019-08-28 17:52:31 -0500

    init

---
+++ includes/meetings.php
@@ -0,0 +1,163 @@
+<?php
+
+class DT_Meetings{
+    private static $_instance = null;
+    public static function instance() {
+        if ( is_null( self::$_instance ) ) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    } // End instance()
+
+    public function __construct() {
+        add_action( 'after_setup_theme', [ $this, 'after_setup_theme' ], 100 );
+        add_action( 'p2p_init', [ $this, 'p2p_init' ] );
+        add_action( 'dt_details_additional_section', [ $this, 'dt_details_additional_section' ], 10, 2 );
+
+        add_filter( 'dt_custom_fields_settings', [ $this, 'dt_custom_fields_settings' ], 10, 2 );
+        add_filter( 'dt_details_additional_section_ids', [ $this, 'dt_details_additional_section_ids' ], 10, 2 );
+    }
+
+
+    public function after_setup_theme(){
+        if ( class_exists( 'Disciple_Tools_Post_Type_Template' )) {
+            new Disciple_Tools_Post_Type_Template( "meetings", 'Meetings', 'Meetings' );
+        }
+    }
+
+    public function dt_custom_fields_settings( $fields, $post_type ){
+        if ( $post_type === 'meetings' ){
+            $fields['contact_count'] = [
+                'name' => "Number of contacts",
+                'type' => 'text',
+                'default' => '',
+                'show_in_table' => true
+            ];
+            $fields['group_count'] = [
+                'name' => "Number of groups",
+                'type' => 'text',
+                'default' => '',
+                'show_in_table' => true
+            ];
+            $fields['contacts'] = [
+                'name' => "Contacts",
+                'type' => 'connection',
+                "p2p_direction" => "from",
+                "p2p_key" => "meetings_to_contacts",
+                'p2p_listing' => 'contacts'
+            ];
+            $fields['groups'] = [
+                'name' => "Groups",
+                'type' => 'connection',
+                "p2p_direction" => "from",
+                "p2p_key" => "meetings_to_groups",
+                'p2p_listing' => 'groups'
+            ];
+        }
+        if ( $post_type === 'groups' ){
+            $fields['meetings'] = [
+                'name' => "Meetings",
+                'type' => 'connection',
+                "p2p_direction" => "to",
+                "p2p_key" => "meetings_to_groups",
+                'p2p_listing' => 'meetings'
+            ];
+        }
+        if ( $post_type === 'contacts' ){
+            $fields['meetings'] = [
+                'name' => "Meetings",
+                'type' => 'connection',
+                "p2p_direction" => "to",
+                "p2p_key" => "meetings_to_contacts",
+                'p2p_listing' => 'meetings'
+            ];
+        }
+        return $fields;
+    }
+
+    public function p2p_init(){
+        p2p_register_connection_type([
+            'name' => 'meetings_to_contacts',
+            'from' => 'meetings',
+            'to' => 'contacts'
+        ]);
+        p2p_register_connection_type([
+            'name' => 'meetings_to_groups',
+            'from' => 'meetings',
+            'to' => 'groups'
+        ]);
+    }
+
+    public function dt_details_additional_section_ids( $sections, $post_type = "" ){
+        if ( $post_type === "meetings"){
+            $sections[] = 'contacts';
+            $sections[] = 'groups';
+        }
+        if ( $post_type === 'contacts' || $post_type === 'groups' ){
+            $sections[] = 'meetings';
+        }
+        return $sections;
+    }
+
+    public function dt_details_additional_section( $section, $post_type ){
+        if ($section == "contacts" && $post_type === "meetings"){
+            $post_type = get_post_type();
+            $post_settings = apply_filters( "dt_get_post_type_settings", [], $post_type );
+            $dt_post = DT_Posts::get_post( $post_type, get_the_ID() );
+            ?>
+
+            <label class="section-header">
+                <?php esc_html_e( 'Contacts', 'disciple_tools' )?>
+            </label>
+
+            <?php render_field_for_display( 'contact_count', $post_settings["fields"], $dt_post ) ?>
+
+            <?php render_field_for_display( 'contacts', $post_settings["fields"], $dt_post ) ?>
+
+        <?php }
+
+        if ($section == "groups" && $post_type === "meetings"){
+            $post_type = get_post_type();
+            $post_settings = apply_filters( "dt_get_post_type_settings", [], $post_type );
+            $dt_post = DT_Posts::get_post( $post_type, get_the_ID() );
+            ?>
+
+            <label class="section-header">
+                <?php esc_html_e( 'Groups', 'disciple_tools' )?>
+            </label>
+
+            <?php render_field_for_display( 'group_count', $post_settings["fields"], $dt_post ) ?>
+
+            <?php render_field_for_display( 'groups', $post_settings["fields"], $dt_post ) ?>
+
+        <?php }
+
+        if ($section == "meetings" && $post_type === "contacts"){
+            $post_type = get_post_type();
+            $post_settings = apply_filters( "dt_get_post_type_settings", [], $post_type );
+            $dt_post = DT_Posts::get_post( $post_type, get_the_ID() );
+            ?>
+
+            <label class="section-header">
+                <?php esc_html_e( 'Meetings', 'disciple_tools' )?>
+            </label>
+
+            <?php render_field_for_display( 'meetings', $post_settings["fields"], $dt_post ) ?>
+
+        <?php }
+
+        if ($section == "meetings" && $post_type === "groups"){
+            $post_type = get_post_type();
+            $post_settings = apply_filters( "dt_get_post_type_settings", [], $post_type );
+            $dt_post = DT_Posts::get_post( $post_type, get_the_ID() );
+            ?>
+
+            <label class="section-header">
+                <?php esc_html_e( 'Meetings', 'disciple_tools' )?>
+            </label>
+
+            <?php render_field_for_display( 'meetings', $post_settings["fields"], $dt_post ) ?>
+
+        <?php }
+    }
+}
